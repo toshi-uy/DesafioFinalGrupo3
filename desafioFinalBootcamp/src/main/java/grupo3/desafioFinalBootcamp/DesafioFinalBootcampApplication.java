0c4dd2427958b1a5454f24456854dd54204f2941
toshi-uy <santiago.borgia@sabre.com>
2021-12-17 11:55:49 -0300

    inicializacion del repo y el proyecto

---
+++ desafioFinalBootcamp/src/main/java/grupo3/desafioFinalBootcamp/DesafioFinalBootcampApplication.java
@@ -0,0 +1,13 @@
+package grupo3.desafioFinalBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinalBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFinalBootcampApplication.class, args);
+	}
+
+}
---
+++ desafioFinalBootcamp/src/test/java/grupo3/desafioFinalBootcamp/DesafioFinalBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.desafioFinalBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinalBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
